458a7e873d17b86c16375dce8fa3fc8c55636058
Coconuts-del <6712@holbertonstudents.com>
2024-01-23 19:04:16 +0100

    add const data

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -5,7 +5,8 @@ request(process.argv[2], (error, response, body) => {
     console.error(`Error: ${error.message}`);
     process.exit(1);
   }
-  const Movies = JSON.parse(body).results.filter((film) =>
+  const data = JSON.parse(body);
+  const Movies = data.results.filter((film) =>
     film.characters.includes('https://swapi-api.hbtn.io/api/people/18/')
   );
   // Display the number of movies where WedgeAntilles is present
