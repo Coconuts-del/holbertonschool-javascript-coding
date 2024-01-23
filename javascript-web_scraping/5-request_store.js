df62b39699f8356394c05e286eafdd4dab036880
Coconuts-del <6712@holbertonstudents.com>
2024-01-23 21:27:35 +0100

    gets the contents of a webpage and stores it in a file

---
+++ javascript-web_scraping/5-request_store.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const request = require('request');
+const fs = require('fs');
+request(process.argv[2], (error, response, body) => {
+  if (error) {
+    console.error(`Error: ${error.message}`);
+    process.exit(1);
+  }
+  fs.writeFile(process.argv[3], body, 'utf-8', (err) => {
+    if (err) { console.error(err); }
+  });
+});
