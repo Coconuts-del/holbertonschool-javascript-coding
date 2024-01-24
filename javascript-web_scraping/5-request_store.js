d8ffc39cb296c7c951b682fc28cea7d07dbdc316
Coconuts-del <6712@holbertonstudents.com>
2024-01-24 08:07:13 +0100

    fs in request

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 const request = require('request');
-request(process.argv[2], (error, response, body) => {
+request(process.argv[2], (error, response) => {
   const fs = require('fs');
   if (error) {
     console.error(`Error: ${error.message}`);
     process.exit(1);
   }
-  fs.writeFile(process.argv[3], body, 'utf-8', (err) => {
+  fs.writeFile(process.argv[3], response.body, 'utf-8', (err) => {
     if (err) { console.error(err); }
   });
 });
