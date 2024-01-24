a027513a89169e2ab92bf25db2dc36bd47077ac9
Coconuts-del <6712@holbertonstudents.com>
2024-01-24 09:59:07 +0100

    using writestream

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -1,12 +1,4 @@
 #!/usr/bin/node
 const request = require('request');
 const fs = require('fs');
-request(process.argv[2], (error, response) => {
-  if (error) {
-    console.error(`Error: ${error.message}`);
-    process.exit(1);
-  }
-  fs.writeFile(process.argv[3], response.body, 'utf-8', (error) => {
-    if (error) { console.error(`Erreur during writing: ${error.message}`); }
-  });
-});
+request(process.argv[2]).pipe(fs.createWriteStream(process.argv[3]));
