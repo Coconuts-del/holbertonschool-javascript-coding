ed753e40b499e788847a78e632a94e20b7e67660
Coconuts-del <6712@holbertonstudents.com>
2024-01-24 09:27:03 +0100

    modify test error

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 const request = require('request');
+const fs = require('fs');
 request(process.argv[2], (error, response) => {
-  const fs = require('fs');
   if (error) {
     console.error(`Error: ${error.message}`);
     process.exit(1);
   }
-  fs.writeFile(process.argv[3], response.body, 'utf-8', (err) => {
-    if (err) { console.error(err); }
+  fs.writeFile(process.argv[3], response.body, 'utf-8', (error) => {
+    if (error) { console.error(`Erreur during writing: ${error.message}`); }
   });
 });
