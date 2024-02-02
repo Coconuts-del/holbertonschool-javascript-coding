4566521889e520ccfe587799c3f8a91692d223f5
Coconuts-del <6712@holbertonstudents.com>
2024-02-02 13:27:54 +0100

    update using process

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,12 +1,7 @@
 console.log('Welcome to Holberton School, what is your name?');
 
-process.stdin.on('readable', () => {
-  const name = process.stdin.read();
-  if (name !== null) {
-    process.stdout.write(`Your name is: ${name}`);
-  }
-});
-
-process.stdin.on('end', () => {
-  process.stdout.write('This important software is now closing');
+process.stdin.on('data', (name) => {
+  process.stdout.write(`Your name is: ${name}`);
+  console.log('This important software is now closing');
+  process.exit();
 });
